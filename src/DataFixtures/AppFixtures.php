2fef3de45af5b9ea409057d89c9aab28beea4fbf
javad <javadsaeid8@gmail.com>
2024-10-31 10:38:56 +0330

    add factory and fixtures for creating test data

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\CompanyFactory;
+use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +11,17 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+
+        $user = UserFactory::createMany(3, ['roles' => ['ROLE_USER']]);
+
+        $superAdmin = UserFactory::createOne([
+            'email' => 'superadmin@test.com',
+            'password' => 'password',
+            'roles' => ['ROLE_SUPER_ADMIN'],
+        ]);
+
+        $company = CompanyFactory::createMany(5);
+        $companyAdmin = UserFactory::createMany(5, ['roles' => ['ROLE_COMPANY_ADMIN'] , 'company' => CompanyFactory::random()]);
 
         $manager->flush();
     }
