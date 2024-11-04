ceb7aa3c96f1b67cfb9a8a59dbd3af6886232cf4
javad <javadsaeid8@gmail.com>
2024-11-04 13:23:02 +0330

    add unit test for getCollection of companies api.

--- tests/Functional/CompanyTest.php
+++ tests/Functional/CompanyTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Functional;
 
+use App\Factory\CompanyFactory;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Zenstruck\Browser\Test\HasBrowser;
 use Zenstruck\Foundry\Test\ResetDatabase;
@@ -13,10 +14,22 @@ class CompanyTest extends KernelTestCase
 
     public function testGetCollectionOfCompanies(): void
     {
-        //todo: test all scenarios for company endPoint.
-//        $this->browser()
-//            ->get('/api/companies')
-//            ->assertSuccessful();
+        CompanyFactory::createMany(5);
+        $json = $this->browser()
+            ->get('/api/companies')
+            ->assertJson()
+            ->assertJsonMatches('"hydra:totalItems"', 5)
+            ->assertJsonMatches('length("hydra:member")', 5)
+            ->json()
+        ;
+
+        $this->assertSame(array_keys($json->decoded()['hydra:member'][0]), [
+            "@id",
+            "@type",
+            "id",
+            "name",
+            "users",
+        ]);
     }
 
 }
